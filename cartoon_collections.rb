4c2576918abe3a308a1de9f96e41b78e55ce7661
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 22:10:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water"]
 end
 
 def long_planeteer_calls# code an argument here
