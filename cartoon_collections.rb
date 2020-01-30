7ffa977cfee4719e5faf0172e7b32cb770bcf62b
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 22:04:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves# code an argument here
   # Your code here
-  dwarves = ["Doc","Dopey","Bashful","Grumpy"]
+  dwarves = ["Doc","Dopey","Bashful","Grumpy", "Sneezy", "Sleepy", "Happy"]
+  puts 
 end
 
 def summon_captain_planet# code an argument here
